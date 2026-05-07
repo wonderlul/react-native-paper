bca489c7f5daa8d927e566a331070f1ab72e5e9f
Michal Lul <lulmichal@gmail.com>
2026-05-07 10:27:14 +0200

    fix: error icon size

--- src/components/TextField/TextFieldErrorIcon.tsx
+++ src/components/TextField/TextFieldErrorIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleProp, View, ViewStyle } from 'react-native';
 
-import { ACCESSORY_SIZE, ERROR_ICON_SIZE } from './constants';
+import { ACCESSORY_SIZE } from './constants';
 import { useInternalTheme } from '../../core/theming';
 import type { ThemeProp } from '../../types';
 import Icon from '../Icon';
@@ -17,24 +17,13 @@ const TextFieldErrorIcon = ({
 }: TextFieldErrorIconProps) => {
   const theme = useInternalTheme(themeOverride);
 
-  const $circleStyle: ViewStyle = {
-    width: ACCESSORY_SIZE,
-    height: ACCESSORY_SIZE,
-    borderRadius: ACCESSORY_SIZE / 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: theme.colors.error,
-  };
-
   return (
     <View style={$wrapperStyle}>
-      <View style={$circleStyle}>
-        <Icon
-          source="exclamation-thick"
-          size={ERROR_ICON_SIZE}
-          color={theme.colors.onError}
-        />
-      </View>
+      <Icon
+        source="alert-circle"
+        size={ACCESSORY_SIZE}
+        color={theme.colors.error}
+      />
     </View>
   );
 };
